f7ead574769eabaf068ed8a984d583c474f4574a
Ben Brown <ben@bobthemac.com>
2017-09-27 23:01:09 +0100

    Renamed MainWindows.xaml and added test button code

--- Image Import/ImportImages.xaml.cs
+++ Image Import/ImportImages.xaml.cs
@@ -24,5 +24,10 @@ namespace Image_Import
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
     }
 }
